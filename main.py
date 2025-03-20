98b664d64c405eb50c5e279f0bac489c77b7ce20
muszkin <muszkin@gmail.com>
2025-03-20 16:46:58 +0100

    add argument

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import sys
 from stats import create_report
 
 
 def main():
-    create_report("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        return sys.exit(1)
+    else:
+        book = sys.argv[1]
+        create_report(book)
 
 
 main()
